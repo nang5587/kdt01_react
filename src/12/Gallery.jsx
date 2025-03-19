cbd022bf95d89dcab0b2113a04faf328be117b40
nang5587 <nang5587@donga.ac.kr>
2025-03-19 14:17:52 +0900

    한국관광 수정

--- src/12/Gallery.jsx
+++ src/12/Gallery.jsx
@@ -62,8 +62,10 @@ export default function Gallery() {
                             onClick={()=>{}} /></span>
             </form>
         </div>
+        <div className="flex justify-center items-center">
         <div className="w-11/12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-content-center gap-4">
             {tags}
+        </div>
         </div>
     </div>
   )
