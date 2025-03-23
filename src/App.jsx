735e499f88acbeffdea1377182bff37d0219d91a
nang5587 <nang5587@donga.ac.kr>
2025-03-23 23:53:57 +0900

    배경 수정

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { IoHome } from "react-icons/io5";
 // import MyEffect from "./08/MyEffect";
 // import BoxOffice from "./09/BoxOffice";
 // import Traffic from "./10/Traffic";
-// import MyRef from "./11/MyRef";/
+// import MyRef from "./11/MyRef";
 // import Gallery from "./12/Gallery";
 import Festival from "./13/Festival";
 function App() {
@@ -19,12 +19,15 @@ function App() {
   return (
     <div className='w-full xl:w-8/10 h-screen mx-auto
                     flex flex-col'>
-      <header className="w-full min-h-20 bg-gray-200
+      <header className="w-full min-h-20 bg-linear-to-t from-amber-50 to-sky-200
                          px-10
                          flex justify-between items-center">
         <div className="text-4xl font-bold text-gray-700 flex flex-row">
           {/* <PiFileJsxLight />&nbsp; */}
-          KDT01_React
+          KDT01_React&nbsp;&nbsp;&nbsp;
+          <p className="text-base text-white">
+            ★・・・・・・★・・・・・・★・・・・・・★・・・・・・★・・・・・・★・・・・・・★・・・・・・★
+          </p>
           {/* <div className="flex text-sm items-center mx-5">
             <img src={reactLogo} alt="react" className="w-8" /> + 
             <img src={viteLogo} alt="vite" className="w-8" />
@@ -34,8 +37,8 @@ function App() {
         <IoHome />
         </div>
       </header>
-      <main className="w-full h-full flex-grow
-                      pt-10
+      <main className="w-full h-full flex-grow 
+                      pt-10 bg-amber-50
                       flex flex-col justify-start items-center
                       overflow-y-auto">
       {/* <Hello /> */}
@@ -57,8 +60,8 @@ function App() {
       {/* <Gallery /> */}
       <Festival />
       </main>
-      <footer className="w-full h-20 bg-blue-950 flex justify-center items-center">
-        <h2 className="text-xs text-white text-center">
+      <footer className="w-full h-20  bg-linear-to-t from-sky-200 to-amber-50 flex justify-center items-center">
+        <h2 className="text-xs text-gray-700 font-bold text-center">
           -금정식당 이용안내-<br />
           메뉴선택후(부산대 홈페이지참조) 식권 자판기에서 식권 발권후<br/>
           배식라인에 맞춰 줄을 서주셔야합니다.</h2>
